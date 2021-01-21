130f41556debd746ba265e135980f721d9611326
Alexander Zaitsev <aliaaksandr.zaitsau@solarwinds.com>
2021-01-22 00:41:25 +0300

    feat: add supapro telegram bot
    - add initial implementation
    
    Tested:
    - Several local tests

---
+++ src/commands.rs
@@ -0,0 +1,112 @@
+use crate::parameters;
+use teloxide::{prelude::*, utils::command::BotCommand};
+
+#[derive(BotCommand)]
+#[command(rename = "lowercase", description = "These commands are supported:")]
+pub enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "display info about bot.")]
+    About,
+    #[command(rename = "s", description = "forward a message to supapro.")]
+    Supapro,
+    #[command(rename = "p", description = "forward a message to pro.")]
+    Pro,
+}
+
+pub async fn command_answer(
+    cx: &UpdateWithCx<Message>,
+    command: Command,
+    parameters: std::sync::Arc<parameters::Parameters>,
+) -> anyhow::Result<()> {
+    static HELP_TEXT: &str = "Я просто помогаю с поддержанием порядка в С++ чатах!";
+
+    static ABOUT_TEXT: &str = "По всем замечаниям или предложениям обращаться по этому адресу:\
+        https://github.com/ZaMaZaN4iK/supapro-telegram . Спасибо!";
+
+    match command {
+        Command::Help => {
+            cx.reply_to(HELP_TEXT).send().await?;
+        }
+        Command::About => {
+            cx.reply_to(ABOUT_TEXT).send().await?;
+        }
+        Command::Supapro => {
+            process_forward_command(
+                &cx,
+                parameters.supapro_chat_id,
+                parameters.supapro_chat_username.as_str(),
+            )
+            .await?;
+        }
+        Command::Pro => {
+            process_forward_command(
+                &cx,
+                parameters.pro_chat_id,
+                parameters.pro_chat_username.as_str(),
+            )
+            .await?;
+        }
+    };
+
+    Ok(())
+}
+
+async fn process_forward_command(
+    cx: &UpdateWithCx<Message>,
+    chat_id_to: i64,
+    chat_username_to: &str,
+) -> anyhow::Result<()> {
+    if let Some(user) = cx.update.from() {
+        let status = cx
+            .bot
+            .get_chat_member(cx.update.chat_id(), user.id)
+            .send()
+            .await?
+            .status;
+
+        if status == teloxide::types::ChatMemberStatus::Creator
+            || status == teloxide::types::ChatMemberStatus::Administrator
+        {
+            // 1) Forward reply of this command to a target chat
+            // 2) Delete it from the current chat
+            // 3) Send a message to an original author with a notification
+
+            if let Some(reply) = cx.update.reply_to_message() {
+                let original_author = reply.from();
+
+                cx.bot
+                    .forward_message(chat_id_to, reply.chat.id, reply.id)
+                    .send()
+                    .await?;
+
+                cx.bot
+                    .delete_message(reply.chat.id, reply.id)
+                    .send()
+                    .await?;
+
+                let mut response = format!("Ваш вопрос перемещён в чат @{} . Там Вам с радостью помогут с Вашей проблемой :)", chat_username_to).to_string();
+                if let Some(original_author) = original_author {
+                    if let Some(username) = &original_author.username {
+                        response = format!("@{} {}", username, response);
+                    }
+
+                    cx.bot
+                        .send_message(cx.update.chat_id(), response)
+                        .send()
+                        .await?;
+                } else {
+                    cx.bot
+                        .send_message(cx.update.chat_id(), response)
+                        .send()
+                        .await?;
+                }
+            } else {
+                static MISSING_REPLY: &str = "Пожалуйста, ответьте на сообщение. Спасибо!";
+                cx.reply_to(MISSING_REPLY).send().await?;
+            }
+        }
+    }
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,65 @@
+mod commands;
+mod logging;
+mod parameters;
+mod webhook;
+
+use teloxide::{prelude::*, utils::command::BotCommand};
+
+#[tokio::main]
+async fn main() {
+    run().await;
+}
+
+async fn run() {
+    logging::init_logger();
+    log::info!("Starting Supapro bot");
+
+    let parameters = std::sync::Arc::new(parameters::Parameters::new());
+    let bot_parameters = parameters.clone();
+
+    let bot = Bot::from_env();
+
+    let bot_dispatcher =
+        Dispatcher::new(bot.clone()).messages_handler(move |rx: DispatcherHandlerRx<Message>| {
+            rx.for_each(move |message| {
+                let parameters = bot_parameters.clone();
+                async move {
+                    let message_text = match message.update.text() {
+                        Some(x) => x,
+                        None => return,
+                    };
+
+                    // Handle commands. If command cannot be parsed - continue processing
+                    match commands::Command::parse(message_text, &parameters.bot_name) {
+                        Ok(command) => {
+                            commands::command_answer(&message, command, parameters)
+                                .await
+                                .log_on_error()
+                                .await;
+                            ()
+                        }
+                        Err(_) => (),
+                    };
+                }
+            })
+        });
+
+    if parameters.is_webhook_mode_enabled {
+        log::info!("Webhook mode activated");
+        let rx = webhook::webhook(bot);
+        bot_dispatcher
+            .dispatch_with_listener(
+                rx.await,
+                LoggingErrorHandler::with_custom_text("An error from the update listener"),
+            )
+            .await;
+        return;
+    }
+
+    log::info!("Long polling mode activated");
+    bot.delete_webhook()
+        .send()
+        .await
+        .expect("Cannot delete a webhook");
+    bot_dispatcher.dispatch().await;
+}
